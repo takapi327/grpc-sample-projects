bd3beb17eeab9a42d8766160727c50b92cc47e4b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-21 23:18:10 +0900

    Fix bug

--- jvm/fs2-grpc-example/project/Dependencies.scala
+++ jvm/fs2-grpc-example/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val grpcNetty    = "io.grpc" % "grpc-netty-shaded" % scalapb.compiler.Version.grpcJavaVersion
   val grpcServices = "io.grpc" % "grpc-services" % "1.53.0"
 
-  val logback = "ch.qos.logback" % "logback-classic" % "1.4.6",
+  val logback = "ch.qos.logback" % "logback-classic" % "1.4.6"
 
   val http4s: Seq[ModuleID] = Seq(
     "http4s-dsl",
