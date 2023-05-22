1ff2d938c27d8d4c13170cbd0f1b4b303599d468
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-23 00:33:49 +0900

    Create svelte-grpc-client

---
+++ node/svelte-grpc-client/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+import { handler } from './build/handler.js'
+
+const app = express()
+
+// add a route that lives separately from the SvelteKit app
+app.get('/healthcheck', (req, res) => {
+    res.end('ok');
+})
+
+// let SvelteKit handle everything else, including serving prerendered pages and static assets
+app.use(handler)
+const server = app.listen(9000, () => {})
+
+// Graceful Shutdown
+process.on('SIGINT', function () {
+    // close connections
+    server.close(() => { })
+})
+
+process.on('SIGTERM', function () {
+    // close connections
+    server.close(() => { })
+})
