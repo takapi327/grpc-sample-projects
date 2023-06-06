bc05e0191f2420c1c306c069c1e8d9a792422bf7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-06 23:21:34 +0900

    Fix index.ts

--- node/lambda/index.ts
+++ node/lambda/index.ts
@@ -2,5 +2,5 @@
 exports.handler = (event: any, context: any, callback: any) => {
   console.log(event)
   console.log(context)
-  callback(`{ message: 'Callback By Lambda' }`, 'Call Lambda succeeded')
+  callback(null, { message: 'Callback By Lambda' })
 }
