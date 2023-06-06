e1b0f9068a6f30d3fefee4cf45a3d61f09601462
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-06 22:01:31 +0900

    Create build.sh

---
+++ node/lambda/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo remove dir and zip
+rm -rf dist/
+
+echo compile ts
+tsc -p tsconfig.json
+
+echo copy package.json
+cp -f ./package.json ./dist
+
+echo install module
+cd dist
+pnpm install
+
+echo zip
+zip -r dist.zip ./
